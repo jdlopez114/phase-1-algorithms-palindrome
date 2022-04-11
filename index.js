7371de4a8d3e6b1db4beee3c76f212f4955095d0
Josiel Lopez <jdlopez114@gmail.com>
2022-04-10 22:07:58 -0400

    commit tree 810837fc4bfc21eb8b4210e59319d79485c5a438

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  let reversedWord = word.split("").reverse().join("");
+    if (reversedWord === word){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 /* 
   Add your pseudocode here
+    we need to take in argument word and reverse it, then see if that reversed word equals the regular word 
 */
 
 /*
   Add written explanation of your solution here
+  if we take in the argument word and split it to covert into an array, the call .reverse on it, plus .join to put them back together.
+  then we need to compare them and return a boolean
 */
 
 // You can run `node index.js` to view these console logs
